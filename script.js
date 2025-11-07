29759327aeca5a29aab6046843eb9fac4eb10546
ximenaguell477-oss <ximenaguell477@gmail.com>
2025-11-07 09:28:51 -0600

    html changed

--- script.js
+++ script.js
@@ -1,99 +1,10 @@
 let allWords = [
 
-    "Love",
-
-    "Je t'aime",
-
-    "Te amo",
-
-    "Amour",
-
-    "Amore",
-
-    "Ti amo",
-
-    "Σ'αγαπώ",
-
-    "Ich liebe dich",
-
-    "Te quiero",
-
-    "Eu te amo",
-
-    "Saya cinta kamu",
-
-    "Aishiteru",
-
-    "Wo ai ni",
-
-    "Ani ohevet otcha",
-
-    "Main tumse pyar karta hoon",
-
-    "Ngiyakuthanda",
-
-    "Ek het jou lief",
-
-    "Te iubesc",
-
-    "Volim te",
-
-    "Ti voglio bene",
-
-    "Jeg elsker deg",
-
-    "Jeg elsker dig",
-
-    "Jag älskar dig",
-
-    "Kocham cię",
-
-    "Seni seviyorum",
-
-    "Mahal kita",
-
-    "Amo-te",
-
-    "Mi amas vin",
-
-    "Ik hou van jou",
-
-    "Ani nenu ninnu premisthunnanu",
-
-    "Mo nifẹ rẹ",
-
-    "Ndinokuda",
-
-    "Nakupenda",
-
-    "Taim i ngra leat",
-
-    "Obicham te",
-
-    "Ya tebya lyublyu",
-
-    "Ngo oiy ney",
-
-    "Te sakam",
-
-    "Ua ou te alofa ia te oe",
-
-    "Ke aloha nei au iā oe",
-
-    "Saya sayang awak",
-
-    "Phom rak khun",
-
-    "Chit pa de",
-
-    "Kuv hlub koj",
-
-    "Mi ta stimabo",
-
-    "Is breá liom tú",
-
+    "Love", "Je t'aime", "Te amo", "Amour", "Amore", "Ti amo", "Σ'αγαπώ", "Ich liebe dich","Te quiero","Eu te amo", "Saya cinta kamu","Aishiteru","Wo ai ni","Ani ohevet otcha","Main tumse pyar karta hoon",
+    "Ngiyakuthanda","Ek het jou lief","Te iubesc","Volim te","Ti voglio bene","Jeg elsker deg","Jeg elsker dig","Jag älskar dig","Kocham cię","Seni seviyorum","Mahal kita", "Amo-te",
+    "Mi amas vin","Ik hou van jou","Ani nenu ninnu premisthunnanu","Mo nifẹ rẹ","Ndinokuda","Nakupenda","Taim i ngra leat","Obicham te","Ya tebya lyublyu",
+    "Ngo oiy ney","Te sakam","Ua ou te alofa ia te oe","Ke aloha nei au iā oe","Saya sayang awak","Phom rak khun","Chit pa de","Kuv hlub koj","Mi ta stimabo","Is breá liom tú",
     "Maite zaitut"
-
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -108,15 +19,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     }
 
-    // define variable for animation target
-
     let animate = { word: wordContainer };
 
-    // set CSS property (string is fine)
-
     animate.word.style.setProperty("--particles", String(allWords.length));
-
-    // clear any existing children (optional, safe for reruns)
 
     animate.word.innerHTML = "";
 
